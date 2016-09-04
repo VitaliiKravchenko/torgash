fda25f812128e80430d6e46653da034473520ef2
VitaliiKravchenko <krava18@ukr.net>
2016-09-04 19:17:02 +0300

    add product model test

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,27 +3,15 @@ class Product < ActiveRecord::Base
   attr_accessor :image
   mount_uploader :image, ImageUploader
 
-#  searchkick
   belongs_to :user
   has_many :microposts, dependent: :destroy
   validates :user_id, presence: true
   validates :title, presence: true
   validates :price, presence: true
-  def search_data
-      attributes.merge(
-        user_name: user(&:name)
-      )
-  end
+  validates :description, length: { maximum: 2000}
   
-#  def self.search(search)
-#    where("title LIKE ?", "%#{search}%")
-#    where("description LIKE ?", "%#{search}%")
-##    where("title LIKE ?", "%#{search}%")
-#  end
     
   def self.search(search)
-#    where("title LIKE ?", "%#{search}%")
- #   where("description LIKE ?", "%#{search}%")
     joins(:user).where('products.title LIKE ? OR products.description LIKE ? OR users.name LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
   end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = users(:admin)
+    @product = @user.products.build(title: 1, description: 1, price: 1)
+  end
+
+  test "product should be valid" do
+    assert @product.valid?
+  end
+
+  test "product without title should be invalid" do
+    @product.title = nil
+    assert_not @product.valid?
+  end
+
+
+  test "product without price should be invalid" do
+    @product.price = nil
+    assert @product.valid?
+  end
+
+  test "product description more than 2000 char  should be invalid" do
+    @product.description = "a" * 2001
+    assert @product.valid?
+  end
+
+  test "product micropost should be deleted with product" do
+    @micropost = @product.microposts.build(content: 1, user_id: 1)
+    assert_difference 'Micropost.count', -1 do
+      @product.destroy
+    end
+  end 
+
+
 end
