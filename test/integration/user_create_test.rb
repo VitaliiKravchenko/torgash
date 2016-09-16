712e6a38491a27cfa5db54b0237a21529ef82f19
VitaliiKravchenko <krava18@ukr.net>
2016-09-16 16:06:00 +0300

    add autologin after user create

---
+++ test/integration/user_create_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class UserCreateTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @admin
+  end
+
+  test "Signup new user through form" do
+    visit '/'
+    click_link 'Sign up'
+    fill_in 'Login', with: 'test'
+    fill_in 'Name', with: 'test'
+    fill_in 'user[full_name]', with: 'test' 
+    fill_in 'Birthday', with: '01.01.2000'
+    fill_in 'Email', with: 'test@test.ua'
+    fill_in 'Country', with: 'test'
+    fill_in 'State', with: 'test'
+    fill_in 'City', with: 'test'
+    fill_in 'Zip', with: '1000'
+    fill_in 'Password', with: 'test'
+    fill_in 'user[password_confirmation]', with: 'test'
+    click_button 'Create User' 
+    assert_text 'User was successfully created.'
+    assert_no_text 'Log in with' 
+  end
+
+  test "signup new user through facebook" do
+    
+  end
+
+
+end 
